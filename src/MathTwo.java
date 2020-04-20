02f549aa0357d4e8d320d62dee4a5cc0f5d656c7
rap9210 <rap9210@gmail.com>
2020-04-20 15:09:24 -0400

    MathTwo

---
+++ src/MathTwo.java
@@ -0,0 +1,8 @@
+public class MathTwo {
+    public static void main(String[] args){
+        int my_number = 2;
+        double my_other_number = 1.7938;
+        System.out.println(my_number);
+        System.out.println(my_other_number);
+    }
+}

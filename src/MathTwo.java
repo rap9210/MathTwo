5f408b4bc71f2e3d50a6170a83f999d4f6c2d504
rap9210 <rap9210@gmail.com>
2020-04-20 16:11:43 -0400

    MathTwo + Comments

--- src/MathTwo.java
+++ src/MathTwo.java
@@ -1,7 +1,10 @@
 public class MathTwo {
     public static void main(String[] args){
+        //initialize and assign value to my int variable
         int my_number = 2;
+        //initialize and assign value to my double variable
         double my_other_number = 1.7938;
+        //print them out
         System.out.println(my_number);
         System.out.println(my_other_number);
     }
